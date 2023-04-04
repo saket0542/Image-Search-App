fd88a8ab5c1552ae5a9429525ddc96cfbc532f42
saket saraogi <saketsaraogi1@gmail.com>
2023-04-05 01:45:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import ImageComponent from "./components/ImageComponent";
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const Header = styled.div`
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;
+`;
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+const LogoImage = styled.img`
+  width: 48px;
+  height: 48px;
+  margin: 15px;
+  border-radius:10px;
+`;
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
+const ImageListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 25px;
+  justify-content: space-evenly;;
+`;
+const Placeholder = styled.img`
+  width: 120px;
+  height: 120px;
+  margin: 150px;
+  opacity: 50%;
+`;
 
 function App() {
+  const [searchQuery, updateSearchQuery] = useState("");
+  const [imageList, updateImageList] = useState([]);
+  const [timeoutId, updateTimeoutId] = useState();
+  const [mode,setMode]=useState("light")
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode
+    },
+  });
+  const fetchData=async(searchString)=>{
+      const response=await axios.get(
+      `https://api.unsplash.com/search/photos?page=1&query=${searchString}&client_id=xQ-DWfk84120yW4mpt2O-grHnWomaD5GvCvdHtLBwts`
+      )
+      updateImageList(response.data.results);
+     console.log(response.data.results)
+  }
+  const onTextChange=(e)=>{
+      clearTimeout(timeoutId);
+      updateSearchQuery(e.target.value);
+      const timeout=setTimeout(()=>fetchData(e.target.value),500);
+      updateTimeoutId(timeout);
+  }
+  const label = { inputProps: { 'aria-label': 'Color switch demo' } };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+    <Container bgcolor={"background.default"} color={"text.primary"}>
+      <Header>
+        <AppName>
+          <LogoImage src="https://thumbs.dreamstime.com/z/creative-colorful-camera-logo-design-symbol-vector-illustration-150574893.jpg" />
+          Image Search Gallery
+        </AppName>
+        <SearchBox>
+          <SearchIcon src="https://i.pinimg.com/736x/fa/0e/7b/fa0e7b992eff03c576727e95c746005c.jpg" />
+          <SearchInput
+            placeholder="Search Images"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
+        </SearchBox>
+        <FormGroup>
+      <FormControlLabel control={<Switch onClick={(e)=>setMode(mode==="light"?"dark":"light")}  {...label} defaultUnChecked />} label="DarkMode" />
+    </FormGroup>
+      </Header>
+      <ImageListContainer>
+      {
+        imageList?.length?(imageList.map((img)=>
+          <ImageComponent
+            image={img}
+            key={img.id}
+            id={img.id}
+          />
+        )):(
+          <Placeholder src="https://play-lh.googleusercontent.com/NuJSG_bIoce6kvvtJnULAf34_Rsa1j-HDEt4MWTOrL_3XA51QH9qOQR5UmAYxPI96jA" />
+        )
+      }
+      </ImageListContainer>
+    </Container>
+    </ThemeProvider>
   );
 }
 
